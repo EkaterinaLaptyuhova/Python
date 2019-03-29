6dde5c2d036605d829901ad0c82cc7bea19130d9
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-03-29 17:25:09 +0300

    Exercice 1-2.

--- Ex.py
+++ Ex.py
@@ -1,1 +1,8 @@
-test
+print("Hello world!")
+sec = 42*60 + 42
+print("There are ", sec, "seconds in 42 minutes and 42 seconds.")
+miles = 10/1.61
+print("There are", miles, "miles in 10 kilometers")
+print(miles/sec, "is a speed mile per sec;", miles/(sec/60), "is a speed miles per min", miles/(sec/3600), "is a speed miles per hour")
+
+
