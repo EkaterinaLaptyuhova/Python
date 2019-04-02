9b1577cf33224db79e02383a3e495506966f68ec
EkaterinaLaptyuhova <klaptyuhova@gmail.com>
2019-04-02 12:19:35 +0300

    Chapter 4, Exercise 1.

--- Turtule Module.py
+++ Turtule Module.py
@@ -5,6 +5,19 @@ bob = turtle.Turtle()
 print(bob)
 bob.speed(1)
 
-for i in range (4):
-    bob.fd(100)
-    bob.lt(90)
+
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+
+square(bob)
+
+def second_square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+
+second_square(bob, 120)
