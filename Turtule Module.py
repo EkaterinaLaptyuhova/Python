5214661ef271467e01f35a3ae2c9bd97eda68d74
EkaterinaLaptyuhova <klaptyuhova@gmail.com>
2019-04-02 12:25:07 +0300

    Chapter 4, Exercise 1.

--- Turtule Module.py
+++ Turtule Module.py
@@ -10,11 +10,12 @@ def square(t):
     for i in range(4):
         t.fd(100)
         t.lt(90)
-
+    bob.reset()
 
 square(bob)
 
 def second_square(t, length):
+    bob.speed(1)
     for i in range(4):
         t.fd(length)
         t.lt(90)
