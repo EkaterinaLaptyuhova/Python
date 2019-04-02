8f009ffcce40bde00dd803b20965230ab75f3eb2
EkaterinaLaptyuhova <klaptyuhova@gmail.com>
2019-04-02 13:09:39 +0300

    Chapter 4, Exercise 1 (+polygon).

--- Turtule Module.py
+++ Turtule Module.py
@@ -19,6 +19,18 @@ def second_square(t, length):
     for i in range(4):
         t.fd(length)
         t.lt(90)
+    bob.reset()
 
 
 second_square(bob, 120)
+
+
+def polygon(t, n, length):
+    bob.speed(1)
+    for i in range(n):
+        t.fd(length)
+        t.lt(360/n)
+    bob.reset()
+
+
+polygon(bob, 6, 35)
