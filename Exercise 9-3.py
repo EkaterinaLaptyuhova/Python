e04a09178f2e3bada0646961699727de652ba8c7
EkaterinaLaptyuhova <klaptyuhova@gmail.com>
2019-04-09 21:30:36 +0300

    Chapter 9, Exercise 9-5.

--- Exercise 9-3.py
+++ Exercise 9-3.py
@@ -1,6 +1,9 @@
 fin = open('/Users/user/Documents/words.txt')
 
 
+"""
+print("Exercise 9-3")
+
 
 def avoid(avoid_symbols):
     count_of_words = 0
@@ -19,3 +22,24 @@ def avoid(avoid_symbols):
 
 characters = input("Write avoid symbols: ")
 avoid(characters)
+"""
+
+
+print("Exercise 9-5")
+
+
+def uses_all(possible_symboles):
+    for line in fin:
+        word = line.strip()
+        i = 0
+        while i < len(possible_symboles):
+            if possible_symboles[i] in word:
+                i = i + 1
+            else:
+                break
+        if i == len(possible_symboles) and i == len(word):
+            print(word)
+
+
+possible = input("Write possible symbols: ")
+uses_all(possible)
