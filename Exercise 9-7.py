71c95bb1db57c936e6f49cab5306e28b8b772809
Ekaterina Laptyuhova <klaptyuhova@gmail.com>
2019-04-11 19:19:14 +0300

    Chapter 9, Exercise 9-7.

--- Exercise 9-7.py
+++ Exercise 9-7.py
@@ -1,6 +1,6 @@
 fin = open('D:/words1.txt')
 
-
+"""
 def fun(text):
     for line in fin:
         word = line.strip()
@@ -20,4 +20,27 @@ def fun(text):
 
 
 fun(fin)
+"""
 
+def new_fun(text):
+    for line in fin:
+        word = line.strip()
+        i = 0
+        count = 0
+        while i < len(word) - 1:
+            while word[i] == word[i+1]:
+                i = i + 1
+                return i
+            k = i
+            while word[k] != word[k+1]:
+                k = k + 1
+                return k
+            if i + 1 == k:
+                count = count + 1
+            else:
+                count = 0
+            if count == 3:
+                print(word)
+
+
+new_fun(fin)
